4749c079fb5c2ded6a2130603ceb7c0edb917d7b
sebastienlarivee <lariveesebastien@gmail.com>
2023-11-16 14:47:22 -0500

    Updated thumbnail generation
    Outputs .png now, dynamically resizes it to be 2mb

--- code/image_tools.py
+++ code/image_tools.py
@@ -1,44 +1,7 @@
 from PIL import Image
 import os
+import io
 import numpy as np
-
-
-class ImageResizer:
-    def __init__(self, input_path):
-        self.input_path = input_path
-
-    def resize_image(self):
-        # Load the image
-        image = Image.open(self.input_path)
-        # Get the dimensions of the original image
-        original_width, original_height = image.size
-        # Compute the dimensions of the resized image
-        resized_width = int(original_width * 0.8)
-        resized_height = int(original_height * 0.8)
-        # Resize the image
-        resized_image = image.resize((resized_width, resized_height))
-        return resized_image
-
-    def save_resized_image(self, output_path):
-        resized_image = self.resize_image()
-        # Convert the image to RGB mode if it's in RGBA mode
-        if resized_image.mode == "RGBA":
-            resized_image = resized_image.convert("RGB")
-        # Save the resized image in JPEG format
-        resized_image.save(output_path, "JPEG")
-
-    def save_resized_image_same_location(self):
-        # Get the base name of the input file
-        base, _ = os.path.splitext(self.input_path)
-        # Construct the output file path
-        output_path = f"{base}_thumbnail.jpg"
-        # Save the resized image
-        self.save_resized_image(output_path)
-
-
-# Usage within another program:
-# resizer = ImageResizer('input.png')
-# resizer.save_resized_image_same_location()
 
 
 class ImageCropper:
@@ -88,3 +51,41 @@ class ImageCropper:
 # Usage within another program:
 # cropper = ImageCropper('input.png', 1080)
 # cropper.save_cropped_image_same_location()
+
+
+class ImageResizer:
+    def __init__(self, input_path):
+        self.input_path = input_path
+
+    def resize_image(self, scale_factor):
+        image = Image.open(self.input_path)
+        resized_width = int(image.width * scale_factor)
+        resized_height = int(image.height * scale_factor)
+        resized_image = image.resize((resized_width, resized_height))
+        return resized_image
+
+    def save_resized_image(self, output_path, max_size_in_mb=2):
+        scale_factor = 1.0
+        print("Thumbnail resizer working...")
+        while True:
+            resized_image = self.resize_image(scale_factor)
+
+            with io.BytesIO() as buffer:
+                resized_image.save(buffer, "PNG")
+                size_in_mb = len(buffer.getvalue()) / (1024 * 1024)
+                if size_in_mb <= max_size_in_mb:
+                    with open(output_path, "wb") as f:
+                        f.write(buffer.getvalue())
+                    break
+
+            scale_factor *= 0.98  # Reduce size by 2% and try again
+
+    def save_resized_image_same_location(self):
+        base, _ = os.path.splitext(self.input_path)
+        output_path = f"{base}_thumbnail.png"
+        self.save_resized_image(output_path)
+
+
+# Example usage
+# resizer = ImageResizer("path/to/image.jpg")
+# resizer.save_resized_image_same_location()
--- code/main.py
+++ code/main.py
@@ -6,7 +6,7 @@ from video_creator import *
 from yt_description import *
 
 # Specify folder that contains /wavs, /pngs, available.json, and used.json
-main_folder_path = r"resources"
+main_folder_path = "resources"
 
 # Create folder for the new album and its metadata
 manager = MetadataManager(main_folder_path)
@@ -20,8 +20,8 @@ organizer.add_length_metadata()
 # Create a square and thumbnail version of the .png
 cropper = ImageCropper(rect_image_path, 1080)
 cropper.save_cropped_image_same_location()
-resizer = ImageResizer(rect_image_path)
-resizer.save_resized_image_same_location()
+thumbnail = ImageResizer(rect_image_path)
+thumbnail.save_resized_image_same_location()
 
 # Create video for Youtube
 video_creator = VideoCreator(album_folder_path)
@@ -30,6 +30,3 @@ video_creator.create_video()
 # Generate Youtube description + chapters -> automatically copy to clipboard
 description = YTDescription(album_folder_path)
 description.new_description()
-
-
-# Upload to DistroKid with selenium
